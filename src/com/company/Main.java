9f82739fe63505379d989fa26bd8f183e586f30b
mmdinu <75750050+mmdinu@users.noreply.github.com>
2021-03-23 20:16:29 +0200

    Aceasta ar trebui sa fie modificarea

---
+++ src/com/company/Main.java
@@ -0,0 +1,59 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        float x = 2.35f;
+        double z = 4.5123423425;
+
+        char e = 'x';
+
+        double suma;
+        suma = x * z;
+
+        System.out.println("Valoarea lui x este: " + x + ", iar valoarea lui z este: "
+                + z +  " si valoarea lui e este: " + e + " , iar suma este: " + suma);
+
+        System.out.println("Gata exertitiul !");
+
+
+
+        // Ex. cu boolean
+
+        System.out.println("Exertitiul cu " +
+                "bolean ! ");
+
+        int num1=10;
+        int num2=20;
+
+        boolean b1=true;
+        boolean b2=false;
+
+        if(num1<num2)
+        { System.out.println(b1); }
+        else
+        { System.out.println(b2); }
+
+        // Ex 2 cu boolean
+
+        System.out.println("Exertitiul 2 cu boolean !");
+
+        System.out.println("Este fals sau adevarat ?");  boolean c1=true;
+        System.out.println("Este fals sau adevarat ?");  boolean c2=false;
+        System.out.println("Este fals sau adevarat ?");  boolean c3=(c1==c2);
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+
+
+
+
+
+
+
+
+    }
+}
